539ae37088824c9928e00e8c4a2f5ae114347f45
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-07-11 12:09:35 +0700

    Update coccoc.bat

--- coccoc.bat
+++ coccoc.bat
@@ -1,4 +1,11 @@
-﻿@echo off
+@echo off
+REM Check for admin privileges
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~dpnx0' -Verb RunAs"
+    EXIT /B
+)
 PowerShell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
 PowerShell.exe -Command "irm https://go.bibica.net/coccoc  | iex"
 pause
