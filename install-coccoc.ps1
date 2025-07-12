108393b42cf601274254e5d29bfb394ea2e16965
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-07-13 00:33:39 +0700

    Update install-coccoc.ps1

--- install-coccoc.ps1
+++ install-coccoc.ps1
@@ -19,7 +19,7 @@ $ProgressPreference = 'SilentlyContinue'
 # Require Administrator privileges
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     if ([string]::IsNullOrEmpty($PSCommandPath)) {
-        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://go.bibica.net/chromium | iex`"" -Verb RunAs
+        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://go.bibica.net/coccoc | iex`"" -Verb RunAs
     } else {
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     }
