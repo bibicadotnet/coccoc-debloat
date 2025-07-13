7b3c69627518aba51878fe105dcc45badc3ebfb7
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-07-13 09:13:17 +0700

    Update profile.bat

--- profile.bat
+++ profile.bat
@@ -1,4 +1,11 @@
 @echo off
+REM Check for admin privileges
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~dpnx0' -Verb RunAs"
+    EXIT /B
+)
 PowerShell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
-PowerShell.exe -Command "irm https://raw.githubusercontent.com/bibicadotnet/coccoc-debloat/refs/heads/main/profile.ps1  | iex"
+PowerShell.exe -Command "irm https://go.bibica.net/coccoc-profile | iex"
 pause
